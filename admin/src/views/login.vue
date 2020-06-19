9cf894c4f8cc00c58298f4587203e982afe50e49
Siyi Guo <siyi.guo@grabtaxi.com>
2020-06-20 01:18:58 +0800

    (CHORE) 增加登录页面的点击事件，跳转到admin页面

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -48,7 +48,7 @@
                             <span class="lbl">记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button @click="login()" type="button" class="width-35 pull-right btn btn-sm btn-primary">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
@@ -205,6 +205,11 @@
   $('body').attr('class', 'login-layout light-login');
   export default {
     name: 'Login',
+    methods: {
+      login() {
+        this.$router.push("/admin");
+      }
+    }
   }
 </script>
 
