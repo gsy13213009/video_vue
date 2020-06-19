c96c09c65b81c24d899f46682cc476a9489ec9ac
Siyi Guo <siyi.guo@grabtaxi.com>
2020-06-19 10:19:50 +0800

    (CHORE) 将system注册到eureka

--- system/src/main/java/com/gsy/system/SystemApplication.java
+++ system/src/main/java/com/gsy/system/SystemApplication.java
@@ -2,8 +2,10 @@ package com.gsy.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
 
     public static void main(String[] args) {
